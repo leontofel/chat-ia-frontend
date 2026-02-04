1c827c13a264e448a3475ca196e3359fc094480b
Leonardo Tofel <leo.tofel@gmail.com>
2026-02-04 19:04:40 -0300

    fix: quasar config for build

--- quasar.config.ts
+++ quasar.config.ts
@@ -40,7 +40,7 @@ export default defineConfig((/* ctx */) => {
         browser: [ 'es2022', 'firefox115', 'chrome115', 'safari14' ],
         node: 'node20'
       },
-
+      publicPath: '/chat/',
       typescript: {
         strict: true,
         vueShim: true
